762ae9b726c7a32e952f3a772d04286ade3cb263
arjangeloknarren-sys <arjangeloknarren@gmail.com>
2025-09-04 22:57:08 +0200

    Update main.php

--- test-project/main.php
+++ test-project/main.php
@@ -1,13 +1,31 @@
 <?php
-/*-------------------------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
- *-------------------------------------------------------------------------------------------------------------*/
-
-function hello($name) {
-	echo "Hello $name!";
-}
-
-hello('remote world');
-
+// Startwaarde instellen in PHP
+$start = 100;
 ?>
+
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Aftellen van 100 naar 0</title>
+    <script>
+        window.onload = function() {
+            let i = <?php echo $start; ?>; // Startwaarde uit PHP
+            let output = document.getElementById("countdown");
+
+            let interval = setInterval(function() {
+                output.innerHTML = i; // Toon huidige waarde
+                i--;
+
+                if (i < 0) {
+                    clearInterval(interval); // Stop wanneer 0 bereikt is
+                }
+            }, 1000); // 1000 ms = 1 seconde
+        }
+    </script>
+</head>
+<body>
+    <h1>Aftellen van 100 naar 0</h1>
+    <div id="countdown" style="font-size:2em; font-weight:bold;"></div>
+</body>
+</html>
